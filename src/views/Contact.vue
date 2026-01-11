920c695fd4af74bac917a0b26467d22397626f2e
ชืichoritsme <ichor2718@gmail.com>
2026-01-11 16:46:36 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,9 @@
+<template lang="">
+    <div>
+        <h1>Contact to Teeradon Suphapol</h1>
+        <h3>Tel.039-324-594</h3>    
+        <img alt="chicken" src="../assets/chicken.jpg">
+
+    </div>
+</template>
+

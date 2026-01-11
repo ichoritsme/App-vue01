c5df4064688bd8f1aec1e82c55f5e715e6e41405
ชืichoritsme <ichor2718@gmail.com>
2026-01-11 18:27:42 +0700

    update v1

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,8 +1,46 @@
 <template lang="">
-    <div>
-        <h1>Contact to Teeradon Suphapol</h1>
+       <div class="container">
+    <div class="text-primary">
+        
+        <h1 >Contact to Teeradon Suphapol</h1>
         <h3>Tel.039-324-594</h3>    
-        <img alt="chicken" src="../assets/chicken.jpg">
+        <img  alt="chicken" src="../assets/chicken.jpg" width="300">
+
+        
+        <div class="container">
+  <div class="row">
+    <div class="col-sm-6">
+        <img alt="chicken" src="../assets/view.jpg" width="400">
+    </div>
+
+
+
+    <div class="col-sm-6">
+        <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+
+
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+
+
+<div class="mb-3 text-center">
+<input class="btn btn-primary" type="submit" value="Submit">&nbsp; &nbsp;
+<input class="btn btn-danger" type="reset" value="Cancel">
+</div>
+    </div>
+  </div>
+</div>
+
+ 
+        
+</div>
+
+
 
     </div>
 </template>
